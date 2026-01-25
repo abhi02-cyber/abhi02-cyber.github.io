ed035936d5c5dfe798c587f97ff86e0adcad2199
https://amanpal.github.io <as9839786@gmail.com>
2026-01-26 00:31:17 +0530

    Update script.js
    Signed-off-by: https://amanpal.github.io <as9839786@gmail.com>

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// Future me results ya features add karne ke kaam aayega
+// fetch("programs.json")
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById("programList");
+    data.forEach(item => {
+      list.innerHTML += `
+        <div class="program-card">
+          <h3>${item.title}</h3>
+          <p>${item.desc}</p>
+          <a href="${item.link}" target="_blank">View More</a>
+        </div>
+      `;
+    });
+  });
+Future me results ya features add karne ke kaam aayega
 console.log("Expert Science Classes website loaded");
